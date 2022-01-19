29855a51209c50af3ea2b7cd9b51fd42c339a8e9
NakRex <khannasimahmad@gmail.com>
2022-01-19 16:16:19 +0530

    competed efitObject function

--- copyObject.js
+++ copyObject.js
@@ -1,8 +1,35 @@
 function editObject(student) {
     let newStudent = Object.create(student);
-    
-    for (let i = 0; i < newStudent.length; i++){
-        if(i==0)
-    }
-
+    let frontend = {skill: 'BootStrap',level: 8};
+    let backend = { skill: 'express', level: 9 };
+    let database = { skill: 'SQL', level: 8 };
+    let datascience = 'SQL';
+    newStudent.skills.frontEnd.push(frontend);
+    newStudent.skills.backEnd.push(backend);
+    newStudent.skills.dataBase.push(database);
+    newStudent.skills.dataScience.push(datascience);
+    return newStudent;
 }
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node',level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+editObject(student);
