5eb7fe96f86c974910653daf8ed7aae18808da49
NakRex <khannasimahmad@gmail.com>
2022-01-19 17:23:46 +0530

    added function for keylength of object

---
+++ objectlength.js
@@ -0,0 +1,29 @@
+function objectLength(student) {
+    let objLen = Object.keys(student).length;
+    return objLen;
+}
+
+
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node',level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+console.log(objectLength(student))
