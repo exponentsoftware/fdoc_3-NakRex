86e64b45e932996c21299360913a215eb81416f2
NakRex <khannasimahmad@gmail.com>
2022-01-19 18:00:48 +0530

    added signUp function

---
+++ signUp.js
@@ -0,0 +1,57 @@
+
+const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+];
+
+
+function signUp(user) {
+    let userEmail = user.email;
+    for (let i = 0; i < users.length; i++){
+        if (users[i].email == userEmail) {
+            return 'user already exist';
+        }
+    }
+    users.push(user);
+    return 'user signed up successfully'
+}
+
+console.log(signUp({ 'username':'nak','name':'nasim', 'email':'nak@nak.com', 'password':'abc123'}));
