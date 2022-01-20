87340bb122c5aaf06d18d3f6c7f9bffad6bb9cb9
NakRex <khannasimahmad@gmail.com>
2022-01-20 15:36:10 +0530

    added function to calculate average rating

--- rateProduct.js
+++ rateProduct.js
@@ -1,4 +1,13 @@
 //Create a function called ***rateProduct*** which rates the product
 function rateProduct(product, rate) {
-   return product.rate = rate;
+    return product.rate.push(rate);
 };
+
+function averageRating(product) {
+    let rating = product.rate;
+    let sum = 0;
+    for (let i = 0; i < rating.length; i++) {
+        sum += rating[i];
+    }
+    return sum / rating.length;
+}
